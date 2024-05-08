3a7c1b8632995de125c6ae259e849bffd989e8d1
Artem NitrOu <akravchyg10@mail.ru>
2024-05-08 23:04:40 +0300

    exactly full ready

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ MAX_USER_STT_BLOCKS = 10
 MAX_USER_TTS_SYMBOLS = 7500
 MAX_USER_GPT_TOKENS = 7500
 
-LOGS = 'logs.txt'  # файл для логов
+LOGS = f'{HOME_DIR}logs.txt'  # файл для логов
 DB_FILE = 'messages.db'  # файл для базы данных
 SYSTEM_PROMPT = [{'role': 'system', 'text': 'Ты веселый собеседник. Общайся с пользователем на "ты" и используй юмор. '
                                             'Поддерживай диалог. Не объясняй пользователю, что ты умеешь и можешь. '
